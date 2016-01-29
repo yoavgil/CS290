0d41c291e9fc85ba3b053f122b13b0e273a8a8d0
Yoav Gil <yuvaduvra@gmail.com>
2016-01-28 18:41:14 -0800

    Started Week 4
    Fixed the closure loop bug in the example.

---
+++ Week 4/fixingClosureLoop.js
@@ -0,0 +1,20 @@
+function buildList(list) {
+    var result = [];
+    for (var i = 0; i < list.length; i++) {
+        (function(x) {
+            var item = 'item' + list[x];
+            result.push( function() {console.log(item + ' ' + list[x])} );
+        })(i);
+    }
+    return result;
+}
+ 
+function testList() {
+    var fnlist = buildList([1,2,3]);
+    // using j only to help prevent confusion - could use i
+    for (var j = 0; j < fnlist.length; j++) {
+        fnlist[j]();
+    }
+}
+
+testList();
