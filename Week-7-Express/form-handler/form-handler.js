a247da15d1cc6583165504f47a5222930e2c13a5
Yoav Gil <yuvaduvra@gmail.com>
2016-02-21 16:31:35 -0800

    Changed port
    Changed port and further cleaned up code

--- Week-7-Express/form-handler/form-handler.js
+++ Week-7-Express/form-handler/form-handler.js
@@ -1,17 +1,18 @@
 var express = require("express");
 var app = express();
-
 var handlebars = require("express-handlebars").create({defaultLayout:"main"});
-
 var bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-
 app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
-app.set("port", 3000);
 
+app.set("port", 3001);
+
+/* Handle incoming POST requests */
 app.post("/", function(req, res) {
+	//Parse the URL query string
 	var qParams = [];
 	for (var param in req.query) {
 		qParams.push({"name":param, "value":req.query[param]});
@@ -19,6 +20,7 @@ app.post("/", function(req, res) {
 	var context = {};
 	context.urlData = qParams;
 
+	//Parse the request body
 	var bodyParams = [];
 	for (var param in req.body) {
 		bodyParams.push({"name":param, "value":req.body[param]});
@@ -28,6 +30,7 @@ app.post("/", function(req, res) {
 	res.render("homepage", context);
 });
 
+/* Hand incoming GET requests; parse the query string */
 app.get("/", function(req, res) {
 	var qParams = [];
 	for (var param in req.query) {
