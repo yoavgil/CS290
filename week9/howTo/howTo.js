6f3bcbe7b321a0cdbc792412c733b250a2d3c4fb
Yoav Gil <yuvaduvra@gmail.com>
2016-03-09 19:24:45 -0800

    How-To Navigation
    Added navigation links between the different pages of the how-to guide,
    and styled these links using CSS. Also changed the port number.

--- week9/howTo/howTo.js
+++ week9/howTo/howTo.js
@@ -6,7 +6,7 @@ app.engine("handlebars", handlebars.engine);
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
-app.set("port", 3000);
+app.set("port", 3001);
 
 /* Serve the requested page */
 
