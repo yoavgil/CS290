22202c3147ebdbe862d327f31a35a3c817786f76
Yoav Gil <yuvaduvra@gmail.com>
2016-03-12 12:27:06 -0800

    Started Database Interactions
    Started working on a workout tracker that will use a database.

---
+++ week10/workoutTracker.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var app = express();
+var handlebars = require("express-handlebars").create({defaultLayout:"main"});
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+
+var pool = mysql.createPool({
+	host: "localhost",
+	user: "student",
+	password: "default",
+	database: "student"
+});
+
+//Handler to setup database table
+//This was given with the assignment
+app.get('/reset-table',function(req,res,next){
+	var context = {};
+	pool.query("DROP TABLE IF EXISTS workouts", function(err){
+		var createString = "CREATE TABLE workouts("+
+		"id INT PRIMARY KEY AUTO_INCREMENT,"+
+		"name VARCHAR(255) NOT NULL,"+
+		"reps INT,"+
+		"weight INT,"+
+		"date DATE,"+
+		"lbs BOOLEAN)";
+		pool.query(createString, function(err){
+			context.results = "Table reset";
+			res.render('home',context);
+		});
+	});
+});
+
+
+
+
+
+app.use(function(req, res) {
+	res.status(404);
+	res.render("404");
+});
+
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500);
+	res.render("500");
+});
+
+app.listen(app.get("port"), function() {
+	console.log("Express started on http://localhost:" + app.get("port") + "; press Ctrl-C to terminate.");
+});
+
+app.set("port", 3000);
