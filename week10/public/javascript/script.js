1653e7a30d3dd80689db79754868aa69acbc9463
Yoav Gil <yuvaduvra@gmail.com>
2016-03-13 14:40:12 -0700

    Submit to Button
    Changed input types from Submit to Button

--- week10/public/javascript/script.js
+++ week10/public/javascript/script.js
@@ -84,13 +84,13 @@ function buildTable(response) {
 		hidden.setAttribute("value", entry.id);
 
 		var edit = document.createElement("input");
-		edit.setAttribute("type", "submit");
+		edit.setAttribute("type", "button");
 		edit.setAttribute("name", "edit");
 		edit.setAttribute("value", "Edit");
 		/*edit.addEventListener("click", editRow);*/
 
 		var del = document.createElement("input");
-		del.setAttribute("type", "submit");
+		del.setAttribute("type", "button");
 		del.setAttribute("name", "delete");
 		del.setAttribute("value", "Delete");
 		del.addEventListener("click", deleteRow);
