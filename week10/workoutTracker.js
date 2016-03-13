196544599cc966840875cb067fc98cb0d1c38029
Yoav Gil <yuvaduvra@gmail.com>
2016-03-13 13:54:46 -0700

    Context date substring
    Testing

--- week10/workoutTracker.js
+++ week10/workoutTracker.js
@@ -70,7 +70,8 @@ app.post("/", function (req, res, next) {
 				context.name = rows[0].name;
 				context.reps = rows[0].reps;
 				context.weight = rows[0].weight;
-				context.date = rows[0].date.substring(0,10);
+				context.date = rows[0].date;
+				context.date = context.date.substring(0, 10);
 				if (rows[0].lbs) {
 					context.lbsChecked = "checked";
 				} else {
